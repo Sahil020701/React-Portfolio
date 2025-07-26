727bb51a11cd2a18b10de972799005705a8f0dca
Sahil020701 <sahilgobade777@gmail.com>
2025-07-26 14:59:20 +0530

    font family change

--- portfolio/src/components/introduction/Introduction.tsx
+++ portfolio/src/components/introduction/Introduction.tsx
@@ -6,12 +6,12 @@ function Introduction() {
       <h2>
         Hi, I'm <span style={{ color: "#3e86cb" }}>Sahil</span>
       </h2>
-      <p>
+      {/* <p>
         I'm a software engineer with a passion for building impactful
         applications.
-      </p>
+      </p> */}
       <p>
-        Hi, I'm Sahil, a Software Engineer passionate about building impactful full-stack applications. I specialize in C#, .NET Core, Angular, and SQL, with hands-on experience in developing client-focused, enterprise-grade solutions and the fintech domain.
+        A Software Engineer passionate about building impactful full-stack applications. I specialize in C#, .NET Core, Angular, and SQL, with hands-on experience in developing client-focused, enterprise-grade solutions and the fintech domain.
       </p>
     </div>
   );
--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'></link>
     <App />
   </StrictMode>,
 )
